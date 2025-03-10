d69a83b4663d0c14f1ba38e3d791a093dbb124b2
katieprogrammes <katiehayman98@gmail.com>
2025-03-10 10:03:10 +0000

    Improved Responsiveness

--- import.py
+++ import.py
@@ -7,12 +7,14 @@ import sys
 def main(file_path):
     with app.app_context():  
         # Import Products from CSV File
+        csv_product_names = set()
         with open(file_path, newline="", encoding="utf-8-sig") as f:
             reader = csv.reader(f)
             for name, vitamin, flavour, price, description in reader:
+                csv_product_names.add(name)
+
                 # Check if Product Already Exists Based On Name
                 product = Product.query.filter_by(name=name).first()
-
                 if product:
                     # If Product Exists, Update Fields that have Changed
                     product.vitamin = vitamin
@@ -25,6 +27,13 @@ def main(file_path):
                     product = Product(name=name, vitamin=vitamin, flavour=flavour, price=price, description=description)
                     db.session.add(product)
                     print(f"Added product: {name}")
+
+        # Delete products that are not in the CSV
+        existing_products = Product.query.all()
+        for product in existing_products:
+            if product.name not in csv_product_names:
+                db.session.delete(product)
+                print(f"Deleted product: {product.name}")
 
         db.session.commit()
 
